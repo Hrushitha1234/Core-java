1f6bb661894e0934a53127444744f8ffd524d789
Manepalli Hrushitha <64897238+Hrushitha1234@users.noreply.github.com>
2023-10-12 09:22:44 +0530

    Add files via upload

---
+++ variables and types/Student.java
@@ -0,0 +1,72 @@
+class Student{
+ public static void main(String[] args){
+  StudentDetails.clgname="ACET";
+  StudentDetails.course="ECE";
+  StudentDetails.time="9:30AM";
+  StudentDetails  sd1=new StudentDetails();
+  StudentDetails  sd2=new StudentDetails();  
+  StudentDetails  sd3=new StudentDetails();
+  StudentDetails  sd4=new StudentDetails();
+  
+  sd1.name="Durga";
+  sd1.gender="Female";
+  sd1.rollnumber=435;
+  sd1.dob=2001;
+
+  System.out.println("Student1 Deatils");
+  System.out.println("sd1.clgname\t\t:" +sd1.clgname);
+  System.out.println("sd1.course\t:" +sd1.course);
+  System.out.println("sd1.time\t\t:" +sd1.time);
+  System.out.println("sd1.name\t\t:" +sd1.name);
+  System.out.println("sd1.gender\t\t:" +sd1.gender);
+  System.out.println("sd1.rollnumber\t\t:" +sd1.rollnumber);
+  System.out.println("sd1.dob\t\t:" +sd1.dob);
+  System.out.println();
+
+
+  sd2.name="Hemanth";
+  sd2.gender="Male";
+  sd2.rollnumber=406;
+  sd2.dob=2001;
+
+  System.out.println("Student2 Deatils");
+  System.out.println("sd2.clgname\t\t:" +sd2.clgname);
+  System.out.println("sd2.course\t:" +sd2.course);
+  System.out.println("sd2.time\t\t:" +sd2.time);
+  System.out.println("sd2.name\t\t:" +sd2.name);
+  System.out.println("sd2.gender\t\t:" +sd2.gender);
+  System.out.println("sd2.rollnumber\t\t:" +sd2.rollnumber);
+  System.out.println("sd2.dob\t\t:" +sd2.dob);
+  System.out.println();
+
+  sd3.name="Sanju";
+  sd3.gender="Female";
+  sd3.rollnumber=419;
+  sd3.dob=2002;
+
+  System.out.println("Student3 Deatils");
+  System.out.println("sd3.clgname\t\t:" +sd3.clgname);
+  System.out.println("sd3.course\t:" +sd3.course);
+  System.out.println("sd3.time\t\t:" +sd3.time);
+  System.out.println("sd3.name\t\t:" +sd3.name);
+  System.out.println("sd3.gender\t\t:" +sd3.gender);
+  System.out.println("sd3.rollnumber\t\t:" +sd3.rollnumber);
+  System.out.println("sd3.dob\t\t:" +sd3.dob);
+  System.out.println();
+
+  sd4.name="sureah";
+  sd4.gender="Male";
+  sd4.rollnumber=400;
+  sd4.dob=2001;
+
+  System.out.println("Student4 Deatils");
+  System.out.println("sd4.clgname\t\t:" +sd4.clgname);
+  System.out.println("sd4.course\t:" +sd4.course);
+  System.out.println("sd4.time\t\t:" +sd4.time);
+  System.out.println("sd4.name\t\t:" +sd4.name);
+  System.out.println("sd4.gender\t\t:" +sd4.gender);
+  System.out.println("sd4.rollnumber\t\t:" +sd4.rollnumber);
+  System.out.println("sd4.dob\t\t:" +sd4.dob);
+  System.out.println();
+ }
+}
---
+++ variables and types/StudentDetails.java
@@ -0,0 +1,10 @@
+class StudentDetails{
+ static String clgname;
+ static String course;
+ static String time;
+ 
+ String name;
+ String gender;
+ long rollnumber;
+ int dob;
+}
